3a52b8a271d039e95aab3e0713f4e2792d8b28f0
brooke hurford and taylor pokoj <brookebhurford@gmail.com>
2016-02-18 14:11:05 -0800

    Add media querie sfor mobile

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// var options = [
+//     {selector: '.thinkGreen', offset: 200, callback: 'alert('fired')' },
+//     {selector: '.joinTeam', offset: 200, callback: 'globalFunction()' },
+//     {selector: '.ourProducts', offset: 200, callback: 'Materialize.fadeInImage(".ourProducts")' },
+//   ];
+//   Materialize.scrollFire(options);
+
+$(document).ready(function(){
+  $(".button-collapse").sideNav();
+});
