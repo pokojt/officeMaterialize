07b9510a5997e5c38830be2ca80ff8d46d90d8f4
brooke hurford and taylor pokoj <brookebhurford@gmail.com>
2016-02-18 15:16:21 -0800

    Add an about page and a jquery pluplugin for a covervid

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,36 @@
-// var options = [
-//     {selector: '.thinkGreen', offset: 200, callback: 'alert('fired')' },
-//     {selector: '.joinTeam', offset: 200, callback: 'globalFunction()' },
-//     {selector: '.ourProducts', offset: 200, callback: 'Materialize.fadeInImage(".ourProducts")' },
-//   ];
-//   Materialize.scrollFire(options);
 
 $(document).ready(function(){
-  $(".button-collapse").sideNav();
+  $('.covervid-video').coverVid(1920, 1080);
+  // $(".button-collapse").sideNav();
+  // var slider = $.fn.fsvs({
+	// 	...
+	// 	endSlide : function(index) {
+	// 		$('<div class="slide"><h2>Slide ' + (index+2) + '</h2></div>').appendTo( $('body') );
+	// 		// reset the nth classes if you need it
+	// 		slider.nthClasses(3);
+	// 		// rebind pagination if you need it
+	// 		slider.addPagination();
+	// 	}
+	// 	...
+	// });
+  // var fsvs = $.fn.fsvs({
+	// 	speed : 1000
+	// });
+  //
+  // var fsvs = $.fn.fsvs({
+	// 	speed : 5000,
+	// 	bodyID : 'fsvs-body',
+	// 	selector : '> .slide',
+	// 	mouseSwipeDisance : 40,
+	// 	afterSlide : function(){},
+	// 	beforeSlide : function(){},
+	// 	endSlide : function(){},
+	// 	mouseWheelEvents : true,
+	// 	mouseDragEvents : true,
+	// 	touchEvents : true,
+	// 	arrowKeyEvents : true,
+	// 	pagination : true,
+	// 	nthClasses : false,
+	// 	detectHash : true
+	// });
 });
