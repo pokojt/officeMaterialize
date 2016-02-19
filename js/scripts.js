889cb351756a337f6af00c4fc4ff2b0c838c41f3
brooke hurford and taylor pokoj <pokojt@gmail.com>
2016-02-18 16:16:31 -0800

    Add flowtext to the about page, tried linking more jquery to get navbar to work without success

--- js/scripts.js
+++ js/scripts.js
@@ -1,36 +1,45 @@
 
-$(document).ready(function(){
+$(document).ready(function($){
   $('.covervid-video').coverVid(1920, 1080);
   $(".button-collapse").sideNav();
-  // var slider = $.fn.fsvs({
-	// 	...
-	// 	endSlide : function(index) {
-	// 		$('<div class="slide"><h2>Slide ' + (index+2) + '</h2></div>').appendTo( $('body') );
-	// 		// reset the nth classes if you need it
-	// 		slider.nthClasses(3);
-	// 		// rebind pagination if you need it
-	// 		slider.addPagination();
-	// 	}
-	// 	...
-	// });
-  // var fsvs = $.fn.fsvs({
-	// 	speed : 1000
-	// });
-  //
-  // var fsvs = $.fn.fsvs({
-	// 	speed : 5000,
-	// 	bodyID : 'fsvs-body',
-	// 	selector : '> .slide',
-	// 	mouseSwipeDisance : 40,
-	// 	afterSlide : function(){},
-	// 	beforeSlide : function(){},
-	// 	endSlide : function(){},
-	// 	mouseWheelEvents : true,
-	// 	mouseDragEvents : true,
-	// 	touchEvents : true,
-	// 	arrowKeyEvents : true,
-	// 	pagination : true,
-	// 	nthClasses : false,
-	// 	detectHash : true
-	// });
+
+  var options = [
+   {selector: '.thinkGreen', offset: 200, callback: 'Materialize.toast("This is our ScrollFire Demo!", 1500 )'}
+  ];
+
+   Materialize.scrollFire(options);
+
 });
+
+
+// var slider = $.fn.fsvs({
+// 	...
+// 	endSlide : function(index) {
+// 		$('<div class="slide"><h2>Slide ' + (index+2) + '</h2></div>').appendTo( $('body') );
+// 		// reset the nth classes if you need it
+// 		slider.nthClasses(3);
+// 		// rebind pagination if you need it
+// 		slider.addPagination();
+// 	}
+// 	...
+// });
+// var fsvs = $.fn.fsvs({
+// 	speed : 1000
+// });
+//
+// var fsvs = $.fn.fsvs({
+// 	speed : 5000,
+// 	bodyID : 'fsvs-body',
+// 	selector : '> .slide',
+// 	mouseSwipeDisance : 40,
+// 	afterSlide : function(){},
+// 	beforeSlide : function(){},
+// 	endSlide : function(){},
+// 	mouseWheelEvents : true,
+// 	mouseDragEvents : true,
+// 	touchEvents : true,
+// 	arrowKeyEvents : true,
+// 	pagination : true,
+// 	nthClasses : false,
+// 	detectHash : true
+// });
